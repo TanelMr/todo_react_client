911e781da558b5f80d8275df03d9d125c411d055
Tanel Märjama <tanel.marjama@khk.ee>
2022-12-06 21:21:08 +0200

    Created component with react-modal for login

---
+++ src/components/Login.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import Modal from 'react-modal';
+
+export default function Login() {
+
+const customStyles = {
+    content: {
+        top: '30%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)',
+    },
+    closeButton: {
+        marginTop: 1,
+    }
+
+};
+
+    let subtitle;
+    const [modalIsOpen, setIsOpen] = React.useState(false);
+
+    function openModal() {
+        setIsOpen(true);
+    }
+
+    function afterOpenModal() {
+        // references are now sync'd and can be accessed.
+        subtitle.style.color = '#f00';
+    }
+
+    function closeModal() {
+        setIsOpen(false);
+    }
+
+    return(
+        <div>
+            <button onClick={openModal}>Log in</button>
+            <Modal
+                isOpen={modalIsOpen}
+                ariaHideApp={false}
+                onAfterOpen={afterOpenModal}
+                onRequestClose={closeModal}
+                style={customStyles}
+                contentLabel="Login modal"
+            >
+
+                <h3>Log in</h3>
+                <form>
+                    <label>
+                        <p>Username</p>
+                        <input type="text" />
+                    </label>
+                    <label>
+                        <p>Password</p>
+                        <input type="password" />
+                    </label>
+                    <button type="submit">Submit</button>
+                </form>
+                <button style={{marginTop:10}} onClick={closeModal}>Close</button>
+            </Modal>
+        </div>
+    )
+}

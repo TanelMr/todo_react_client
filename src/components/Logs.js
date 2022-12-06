8806746ddcd9cf874c1afe1a8708fa6a8bd5d332
Tanel Märjama <tanel.marjama@khk.ee>
2022-12-06 21:21:37 +0200

    Created component with react-modal for logs

---
+++ src/components/Logs.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import Modal from 'react-modal';
+
+export default function Logs() {
+
+    const customStyles = {
+        content: {
+            top: '30%',
+            left: '50%',
+            right: 'auto',
+            bottom: 'auto',
+            marginRight: '-50%',
+            transform: 'translate(-50%, -50%)',
+        },
+    };
+
+    let subtitle;
+    const [modalIsOpen, setIsOpen] = React.useState(false);
+
+    function openModal() {
+        setIsOpen(true);
+    }
+
+    function afterOpenModal() {
+        // references are now sync'd and can be accessed.
+        subtitle.style.color = '#f00';
+    }
+
+    function closeModal() {
+        setIsOpen(false);
+    }
+
+
+    return(
+        <div>
+            <button onClick={openModal}>View logs</button>
+            <Modal
+                isOpen={modalIsOpen}
+                ariaHideApp={false}
+                onAfterOpen={afterOpenModal}
+                onRequestClose={closeModal}
+                style={customStyles}
+                contentLabel="Example Modal"
+            >
+                <h2>Logs:</h2>
+                <button style={{marginTop:10}} onClick={closeModal}>Close</button>
+            </Modal>
+        </div>
+    )
+}

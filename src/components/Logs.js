f26ef9b7fe3235bf692284a7475e1052762743c4
Tanel Märjama <tanel.marjama@khk.ee>
2023-01-11 01:15:31 +0200

    Implemented ToDo list logging

--- src/components/Logs.js
+++ src/components/Logs.js
@@ -1,49 +1,92 @@
-import React from 'react';
-import Modal from 'react-modal';
+import React, { useState } from "react";
+import Modal from "react-modal";
 
 export default function Logs() {
+  const customStyles = {
+    content: {
+      top: "30%",
+      left: "50%",
+      right: "auto",
+      bottom: "auto",
+      marginRight: "-50%",
+      transform: "translate(-50%, -50%)",
+    },
+    table: {
+      padding: 5,
+    },
+  };
 
-    const customStyles = {
-        content: {
-            top: '30%',
-            left: '50%',
-            right: 'auto',
-            bottom: 'auto',
-            marginRight: '-50%',
-            transform: 'translate(-50%, -50%)',
-        },
-    };
+  const [modalIsOpen, setIsOpen] = React.useState(false);
 
-    let subtitle;
-    const [modalIsOpen, setIsOpen] = React.useState(false);
+  function openModal() {
+    setIsOpen(true);
+  }
 
-    function openModal() {
-        setIsOpen(true);
-    }
+  function afterOpenModal() {
+    void getLogs();
+  }
 
-    function afterOpenModal() {
-        // references are now sync'd and can be accessed.
-    }
+  function closeModal() {
+    setIsOpen(false);
+  }
 
-    function closeModal() {
-        setIsOpen(false);
-    }
+  const [logs, setLogs] = useState([]);
 
+  const getLogs = async () => {
+    await fetch(process.env.REACT_APP_API + "logs", {
+      method: "GET",
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setLogs(data);
+      })
+      .catch((error) => console.log(error));
+  };
 
-    return (
-        <div>
-            <button onClick={openModal}>View logs</button>
-            <Modal
-                isOpen={modalIsOpen}
-                ariaHideApp={false}
-                onAfterOpen={afterOpenModal}
-                onRequestClose={closeModal}
-                style={customStyles}
-                contentLabel="Example Modal"
-            >
-                <h2>Logs:</h2>
-                <button style={{marginTop: 10}} onClick={closeModal}>Close</button>
-            </Modal>
-        </div>
-    )
+  return (
+    <div>
+      <button onClick={openModal}>View logs</button>
+      <Modal
+        isOpen={modalIsOpen}
+        ariaHideApp={false}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        style={customStyles}
+      >
+        <h2>Logs:</h2>
+
+        <table>
+          <thead>
+            <tr>
+              <th> Date</th>
+              <th> Method</th>
+              <th> UserID</th>
+              <th> New task value</th>
+              <th> New completed value</th>
+              <th> Old task value</th>
+              <th> Old completed value</th>
+            </tr>
+          </thead>
+          <tbody>
+            {logs.map((innerArray, index) => (
+              <tr key={index}>
+                {innerArray.map((item, index) => (
+                  <td style={customStyles.table} key={index}>
+                    {item}
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+
+        <button onClick={closeModal}>Close</button>
+      </Modal>
+    </div>
+  );
 }
